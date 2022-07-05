7cd256792f157255b55dd037638db3bf344f228a
Debby <thesp72@naver.com>
2022-07-05 22:26:24 +0900

    220705

--- BOJ/2588.py
+++ BOJ/2588.py
@@ -1,2 +1,11 @@
-a, b = int(input()), int(input())
-c, d, e, res = 0, 0, 0, 0
+a, b = int(input()), list(input())
+res, ret = 0, []
+for i in range(len(b)):
+    res = a * int(b[i])
+    print(res)
+    if i == 0:
+        ret.append(res)
+    else:
+        ret.append(res * (10 * i))
+print(i for i in ret)
+print(sum(ret))
